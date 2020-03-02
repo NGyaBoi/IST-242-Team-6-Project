316acd95b31cc8eaa0dcb55f3d763dea7b292889
NGyaBoi <NeelGhosh@protonmail.com>
2020-03-02 17:15:01 -0500

    added heading

--- HundredMeter/src/Main.java
+++ HundredMeter/src/Main.java
@@ -1,6 +1,12 @@
-/*
- * 
- */
+/*********************************************************
+ *Filename: 		Main.java
+ *Author: 			Yifan Feng, Neel Ghosh, Ephrain Govere, Matthew Marone
+ *Section:			IST 242.001 / MIS 307.001
+ *Assignment:		Project
+ *Description:		
+ *Date Created:		2/24/2020
+ *Date Modified:	3/2/2020
+*********************************************************/
 
 public class Main 
 {
