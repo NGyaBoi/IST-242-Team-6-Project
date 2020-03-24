47cf7d93bb9e257d23b915047653175ac72687b5
yqf5150 <57574104+yqf5150@users.noreply.github.com>
2020-03-24 00:10:04 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package MeterDash;
+
+import javax.swing.JFrame;
+
+public class Main
+{
+
+	public static void main(String[] args)
+{
+		// TODO Auto-generated method stub
+		GUi frame = new GUi();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("100 Meter Dash Seach Tool");
+		frame.setVisible(true);
+}
+
+}
