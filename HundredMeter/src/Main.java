f0bea13cd0dc2f5559dfcc0c93d82df06fa641df
NGyaBoi <NeelGhosh@protonmail.com>
2020-04-02 15:13:20 -0400

    deleted automated comment

--- HundredMeter/src/Main.java
+++ HundredMeter/src/Main.java
@@ -7,7 +7,6 @@ public class Main
 
 	public static void main(String[] args)
 {
-		// TODO Auto-generated method stub
 		GUi frame = new GUi();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("100 Meter Dash Seach Tool");
