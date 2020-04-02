3840ddb4e30184d1e03f470f55d3ed84f784dc49
NGyaBoi <NeelGhosh@protonmail.com>
2020-04-02 18:16:35 -0400

    fixed spacing and changed spelling of Title

--- HundredMeter/src/Main.java
+++ HundredMeter/src/Main.java
@@ -4,13 +4,11 @@ import javax.swing.JFrame;
 
 public class Main
 {
-
 	public static void main(String[] args)
-{
+	{
 		GUi frame = new GUi();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("100 Meter Dash Seach Tool");
+		frame.setTitle("100m Dash Seach Tool");
 		frame.setVisible(true);
-}
-
+	}
 }
