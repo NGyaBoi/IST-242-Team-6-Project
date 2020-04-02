1d42326ad47a6c4c222f3a2cb4fadc1db39b421d
Matt <Matt@DESKTOP-15R31MD>
2020-04-02 14:39:45 -0500

    Added Combo Box

---
+++ 100 meter Dash/src/MeterDash/GUi.java
@@ -0,0 +1,70 @@
+package MeterDash;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class GUi extends JFrame implements ActionListener
+{
+	private static final int frame_HEIGHT = 400;
+	private static final int frame_WIDTH = 300;
+	JLabel lblSearchBar = createlabel("Please enter the athlete.");
+	JTextField txtName = textfield();
+	JComboBox athletes;
+
+	public GUi()
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Meter Dash Search Tool");
+		setSize(frame_HEIGHT, frame_WIDTH);
+		
+		String[] athleteNames = {"", "More to be added"};
+		athletes = new JComboBox(athleteNames);
+		
+		JPanel ComboPanel = new JPanel();
+		ComboPanel.setLayout(new GridLayout(1,1));;
+		ComboPanel.add(athletes);
+		add(ComboPanel, BorderLayout.NORTH);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(1,3));
+		JButton enter = new JButton("Enter");
+		JButton exit = new JButton("Exit");
+		JButton clear = new JButton("Clear");
+		buttonPanel.add(clear);
+		buttonPanel.add(enter);
+		buttonPanel.add(exit);
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+	}
+	
+	public void InputPanel()
+	{
+		JPanel InputPanel = new JPanel();
+		Box nameBox = Box.createHorizontalBox();
+		nameBox.add(lblSearchBar);
+		InputPanel.add(nameBox);
+		add(InputPanel, BorderLayout.CENTER);
+	}
+	
+	public JTextField textfield()
+	{
+		JTextField textfield = new JTextField();
+		return textfield;
+	}
+	
+	public JLabel createlabel(String prompt)
+	{
+		JLabel createlabel = new JLabel(prompt);
+		return createlabel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
