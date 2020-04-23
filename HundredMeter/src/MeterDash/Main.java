e14d392a3bf1e02a97808462e1341462eafbe68a
NGyaBoi <NeelGhosh@protonmail.com>
2020-04-22 21:12:58 -0400

    added header to main

--- HundredMeter/src/MeterDash/Main.java
+++ HundredMeter/src/MeterDash/Main.java
@@ -1,5 +1,10 @@
-/*
- * 
+/* Project Name:	Hundred Meter Dash
+ * Team:			6
+ * Authors:			Yifan Feng, Neel Ghosh, Matthew Marone
+ * Section:			IST 242
+ * Description		Pulls data from Men's 100m Dash at the 2016 Summer Olympics
+ * 					and displays them at the user's request.
+ * Date Created:	1/28/2020
  */
 
 package MeterDash;
